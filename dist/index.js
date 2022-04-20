0f07d431fefc8c25c9513c1080430e8cf61687ab
Emmanuel Adiba <eabaagah@gmail.com>
2022-04-20 23:02:36 +0000

    first commit

---
+++ dist/index.js
@@ -0,0 +1,15 @@
+"use strict";
+// Basic types
+let id = 5;
+let company = "Emmanuel";
+let isPublished = true;
+let x = "Hello";
+let ids = [1, 2, 3,];
+let arr = [1, "Hello", true];
+// Tuple
+let person = [1, "Emma", true];
+// Array tuple
+let employee = [
+    [1, "Emma", true],
+    [1, "Sam", false]
+];
---
+++ src/index.ts
@@ -0,0 +1,18 @@
+// Basic types
+let id: number = 5
+let company: string = "Emmanuel"
+let isPublished: boolean = true
+
+let x: any = "Hello"
+
+let ids: number[] = [1,2,3,]
+let arr:any[] = [1, "Hello", true]
+
+// Tuple
+let person: [number, string, boolean] = [1, "Emma", true]
+
+// Array tuple
+let employee: [number, string, boolean][] = [
+    [1, "Emma", true],
+    [1, "Sam", false]
+]
